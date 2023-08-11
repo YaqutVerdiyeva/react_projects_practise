ccd102c435239b47a1f0d41f574ad92f6434a119
YaqutVerdiyeva <yaqutverdiyeva22@gmail.com>
2023-08-11 21:07:30 +0400

    completed reviews

---
+++ 03.reviews/src/App.jsx
@@ -0,0 +1,12 @@
+import Reviews from "./components/Reviews";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reviews</h1>
+      <Reviews />
+    </div>
+  );
+}
+
+export default App;
